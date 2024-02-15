fea886e763a18f6e4e0b8ccad364b7716788cdb8
Akans Udoka <Akansudoka7@gmail.com>
2024-02-15 23:29:38 +0100

    Added story controller

---
+++ StoryBoard/Controllers/StoryController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoryBoard.Data;
+using StoryBoard.Models;
+
+namespace StoryBoard.Controllers
+{
+    public class StoryController : Controller
+    {
+        private readonly AppDbContext appDbContext;
+        public StoryController(AppDbContext appDbContext) 
+        { 
+            this.appDbContext= appDbContext;
+        }
+        public IActionResult Index()
+        {
+            var stories = appDbContext.Stories.ToList();
+
+            return View(stories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Story story)
+        {
+            if (ModelState.IsValid)
+            {
+                appDbContext.Stories.Add(story);
+                appDbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(story);
+        }
+    }
+}
