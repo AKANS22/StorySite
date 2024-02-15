9e2635f95335e421d709e0ec8e8191e28431743c
Akans Udoka <Akansudoka7@gmail.com>
2024-02-15 23:14:45 +0100

    added identity

---
+++ StoryBoard/Areas/Identity/Data/StoryBoardContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StoryBoard.Areas.Identity.Data;
+using StoryBoard.Models;
+
+namespace StoryBoard.Data;
+
+public class StoryBoardContext : IdentityDbContext<StoryBoardUser>
+{
+    public StoryBoardContext(DbContextOptions<StoryBoardContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- StoryBoard/Program.cs
+++ StoryBoard/Program.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using StoryBoard.Data;
+using Microsoft.AspNetCore.Identity;
+using StoryBoard.Models;
+using StoryBoard.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connect = builder.Configuration.GetConnectionString("Connection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connect));
+
+var connet = builder.Configuration.GetConnectionString("StoryBoardContextConnection");
+builder.Services.AddDbContext<StoryBoardContext>(options => options.UseSqlServer(connet));
+
+builder.Services.AddDefaultIdentity<StoryBoardUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<StoryBoardContext>();
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
